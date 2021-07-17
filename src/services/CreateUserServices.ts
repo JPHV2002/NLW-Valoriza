dd20eeb1454d13d4131e5c3938992ab26179ad23
Joao Pedro Hegedus Vellenich <jpvellenich@gmail.comm>
2021-07-17 17:49:27 -0300

    User

---
+++ src/services/CreateUserServices.ts
@@ -0,0 +1,32 @@
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories"
+
+interface IUserRequest{
+    name: string;
+    email: string;
+    admin?: boolean;
+}
+
+class CreateUserServices{
+
+    async execute({name, email, admin}: IUserRequest){
+        const userRepository = getCustomRepository(UsersRepositories) ;
+
+        if(!email){
+            throw new Error("Email incorrect");
+        }
+
+        const userAlreadyExists = await userRepository.findOne({email});
+
+        if(userAlreadyExists){
+            throw new Error("User Already exists");
+        }
+
+        const user = userRepository.create({name, email, admin});
+        await userRepository.save(user);
+        return user;
+    }
+
+}
+
+export {CreateUserServices}
